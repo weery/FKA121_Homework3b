b4ae8d0558869cefddb542993b39578806887dec
Simon Nilsson <simnilss@student.chalmers.se>
2016-12-20 15:15:01 +0100

    Used Mathematica to get a fourier transform of phi (included) and added result to Matlab code.

--- code/Task1/main.m
+++ code/Task1/main.m
@@ -11,19 +11,28 @@ d           = 0.5;
 m           = 1;
 p_0         = sqrt(0.1*2*m);
 x_0         = 0;
+dx          = 1;
+n_points    = 1e3;
 
 
 % ----------- VARIABLES ------------
-x=0:0.01:5;
+x = dx*(0:n_points-1);
+p = 1/(n_points*dx)*(0:n_points-1);
 % Functions handles
 Gaussian_Wave_Packet = @(x)1/(pi*d^2)^(1/4)*exp(-(x-x_0).^2/(2*d^2)).*exp(1i*p_0*(x-x_0)/hbar);
+Gaussian_Fourier_Packet = @(p)(exp(1i*p*x_0 - (d^2*(p_0 + p*hbar).^2)./(2*hbar^2))./((d^(-2))^(1/4)*pi^(1/4)));
 % ----
 
 gauss = Gaussian_Wave_Packet(x);
+fourier = abs(Gaussian_Fourier_Packet(p)).^2;
 prob = abs(gauss).^2;
 momentum = abs(fft(prob)).^2;
 momentum = momentum(1:floor(length(x)/2))*1/length(x);
 
-plot(prob)
+
+figure(1); clf;
+%plot(prob)
+plot(p(1:floor(length(x)/2)), momentum)
 hold on
-plot(momentum)
+plot(p, fourier)
+hold off
