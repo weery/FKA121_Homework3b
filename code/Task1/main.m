427b2f454e0d1bedd794731da35016a796d4d433
Simon Nilsson <simnilss@student.chalmers.se>
2016-12-21 15:39:16 +0100

    Stuff

--- code/Task1/main.m
+++ code/Task1/main.m
@@ -18,37 +18,41 @@ p_0         = sqrt(0.1*2*m);
 x_0         = 0;
 dx          = 0.01;
 n_points    = 1024;
+dp          = 2*pi/(n_points*dx);
 
 
 % ----------- VARIABLES ------------
 % space samples
-x = dx*(0:n_points-1);
+x = x_0 + dx*(0:n_points-1);
 % and the corresponding samples in momentum space
-p = 2*pi/(n_points*dx)*(0:n_points/2);
+p = dp*((0:n_points-1)-n_points/2);
 % ---- Functions handles ----
 Gaussian_Wave_Packet = @(x)1/(pi*d^2)^(1/4)*exp(-(x-x_0).^2/(2*d^2)).*exp(1i*p_0*(x-x_0)/hbar);
-% Fourier transform obtained via Mathematica
+% Fourier transform obtained via Mathematica as the 'Inverse Fourier
+% Transform', due to differences in FT defintion
 Gaussian_Packet_Fourier = @(p)(exp(1i*p*x_0 - (d^2*(p_0 - p*hbar).^2)./(2*hbar^2))./((d^(-2))^(1/4)*pi^(1/4)));
 % ---------------------------------
 
 % Sample-discretize the wave packet function
 wave_packet = Gaussian_Wave_Packet(x)*dx;
-fourier_prob = abs(Gaussian_Packet_Fourier(p)).^2;
+theoretic_prob = abs(Gaussian_Packet_Fourier(p)).^2;
 
-%prob = abs(wave_packet).^2;
-%momentum_prob = abs(fft(wave_packet)).^2;
-%momentum_prob = momentum_prob(1:floor(length(x)/2))*1/length(x);
+prob = abs(wave_packet/dx).^2;
+fft_prob_momentum = abs(fftshift(fft(wave_packet))).^2;
 
-temp = abs(fft(wave_packet))/n_points;
-momentum_prob = temp(1:length(p));
-% Compute double-sided contribution
-momentum_prob(2:end-1) = 2*momentum_prob(2:end-1);
-momentum_prob = momentum_prob.^2;
-
+% Plot prob.distr. in momentum space
 figure(1); clf;
-plot(p, momentum_prob*1e5)
+plot(p/p_0, fft_prob_momentum)
 hold on
-plot(p, fourier_prob)
+plot(p/p_0, theoretic_prob)
 hold off
-xlabel('Momentum')
-legend('fourier prob', 'theoretic prob')
+xlim([-20 20])
+xlabel('Momentum / $p_0$', 'interpreter', 'latex', 'fontsize', 14)
+ylabel('Probability distribution', 'fontsize', 14)
+legend('Numerically obtained via FFT', 'Analytically obtained via FT')
+
+% Plot prob.distr. in normal space
+figure(2); clf;
+plot(x, prob)
+ylabel('Probability Distribution', 'fontsize', 14)
+xlabel('Position / [\AA]', 'interpreter', 'latex', 'fontsize', 14)
