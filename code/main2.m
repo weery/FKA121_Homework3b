8340fb5df19a145e1521d19638b6965d9fcb5ac9
Weery <vicnilss@student.chalmers.se>
2017-01-09 13:51:45 +0100

    Task 2 seems to be correct

--- code/main2.m
+++ code/main2.m
@@ -17,14 +17,14 @@ m           = 1.66/1.6*1e2;
 p_0         = sqrt(0.1*2*m);
 x_0         = 0;
 dx          = 0.1;
-n_points    = 2^10;
+n_points    = 2^11;
 dp          = 2*pi/(n_points*dx);
 dt          = 1;
 
 
 % ----------- VARIABLES ------------
 % space samples
-x = x_0+dx*(0:n_points-1);
+x = x_0+dx*((0:n_points-1)-n_points/2);
 % and the corresponding samples in momentum space
 p = ((0:n_points-1)-n_points/2)*dp;
 % Functions handles
@@ -33,6 +33,7 @@ Potential_Function = @(x) x*0;
 Gaussian_Wave_Time_Evolution=@(x,t) (pi^(1/2)*(d+1i*hbar*t/(m*d))).^(-1/2).*...
     exp(-(x-p_0*t/m).^2/(2*d^2*(1+1i*hbar*t/(m*d^2)))).*...
     exp(i*p_0/hbar*(x-p_0*t/(2*m)));
+Gaussian_Time_Probability=@(x,t) 1/(pi^(1/2)*(d^2+hbar^2*t^2/(m^2*d^2))^(1/2))*exp(-(x-p_0/m*t).^2/(d^2+hbar^2*t^2/(m^2*d^2))); 
 % ----
 step_three=Gaussian_Wave_Packet(x);
 
@@ -50,31 +51,23 @@ ylabel('Probability distribution', 'fontsize', 14)
 title('$\left| \psi (0) \right|^2$', 'interpreter', 'latex', 'fontsize', 18)
 
 % Plot the rest in a separate figure
+plotX=1:n_points;
 figure(2); clf;
-plotHandle_1 = plot(x(1:n_points/2), abs(Gaussian_Wave_Packet(x(1:n_points/2)).^2));
+plotHandle_1 = plot(x(plotX), abs(step_three(plotX).^2));
 hold on
-plotHandle_2 = plot(x(1:n_points/2), abs(Gaussian_Wave_Time_Evolution(x(1:n_points/2),0)).^2,'--');
+plotHandle_2 = plot(x(plotX), abs(Gaussian_Wave_Time_Evolution(plotX,0)).^2,'--');
 hold off
-
-
-
-step_one = step_three;
-step_two = fftshift(fft(step_one.*exp_potential));
-step_three = ifft(ifftshift(inv_pot.*step_two));
-
-
 
 xlabel('Position / [\AA]', 'interpreter', 'latex', 'fontsize', 14)
 ylabel('Probability distribution', 'fontsize', 14)
-title('$\left| \psi (t = 256 \mathrm{fs}) \right|^2$', 'interpreter', 'latex', 'fontsize', 18)
+title('$\left|? \psi (t = 256 \mathrm{fs})? \right|^2$', 'interpreter', 'latex', 'fontsize', 18)
 
 for j=1:n_points/4-1
-    step_one = step_three;
-    
+    step_one = step_three;    
     step_two = fftshift(fft(step_one.*exp_potential));
     step_three = ifft(ifftshift(inv_pot.*step_two));
-    set(plotHandle_1, 'YData', abs(step_three(1:n_points/2).^2))
-    set(plotHandle_2, 'YData', abs(Gaussian_Wave_Time_Evolution(x(1:n_points/2),j*dt)).^2)
+    set(plotHandle_1, 'YData', abs(step_three(plotX).^2))
+    set(plotHandle_2, 'YData', Gaussian_Time_Probability(x(plotX),j*dt))
     pause(0.01)
 end
 
